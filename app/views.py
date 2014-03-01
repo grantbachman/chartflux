6ff1a9021c95d62feec6d88b4402c66c4df33b8b
Grant David Bachman <grantbachman@gmail.com>
2014-03-01 16:39:34 -0500

    Check if ticker is in the NYSE or NASDAQ

--- app/views.py
+++ app/views.py
@@ -1,8 +1,15 @@
 from app import myApp
+from flask import render_template, url_for, redirect
 
 @myApp.route('/')
+def root():
+	return redirect(url_for('index'))
+
 @myApp.route('/index')
 def index():
-    return "Hello, world."
+	return render_template('index.html')
 
-
+@myApp.route('/chart/<ticker>')
+def chart(ticker):
+	ticker = ticker.upper()
+	return render_template('index.html', ticker=ticker)
