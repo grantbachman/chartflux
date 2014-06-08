1e7b3378be64269657f3e74a709258e363e5117a
Grant David Bachman <grantbachman@gmail.com>
2014-06-08 14:29:45 -0400

    Remove csrf stuff, not needed yet.

--- app/forms.py
+++
@@ -1,6 +0,0 @@
-from wtforms import Form, TextField
-from wtforms import validators
-
-class TickerForm(Form):
-	ticker = TextField('Ticker', validators=[validators.Length(max=6),
-																validators.DataRequired()])
--- app/views.py
+++ app/views.py
@@ -2,9 +2,7 @@ import sys, os
 import datetime as dt
 from app import myApp
 from models import Stock
-from forms import TickerForm
 from jinja2 import Markup
-from flask.ext.wtf import Form
 from flask import render_template, url_for, redirect, request, flash,\
                   session, abort
 
@@ -21,7 +19,8 @@ def d3():
 def show_stock(stock):
   stock = Stock(stock)
   if not stock.is_valid:
-    return redirect(url_for('index'))
+    error = "Invalid ticker symbol."
+    return render_template('index.html', error = error)
   else:
     end = dt.date.today()
     start = end - dt.timedelta(days = 5)
@@ -36,43 +35,11 @@ def show_stock(stock):
     # need to redirect instead
     return render_template('chart.html', stock=stock, data=format_data)
 
+@myApp.route('/stock/verify', methods = ['POST'])
+def verify_stock():
+  ticker = request.form['ticker']
+  return redirect(url_for('show_stock', stock=ticker))
 
-@myApp.route('/index', methods = ['GET', 'POST'])
+@myApp.route('/index')
 def index():
-  form = TickerForm()
-  if request.method == 'POST':
-    if form.validate == False:
-      flash('Enter a valid stock symbol')
-      return render_template('index.html',form=form)
-    else:
-      ticker = request.form['ticker']
-      stock = Stock(ticker)
-      end = dt.date.today()
-      start = end - dt.timedelta(days = 5)
-      stock.set_data(start, end)
-      format_data = stock.data\
-                         .reset_index()\
-                         .to_json(date_format='iso', orient='index')
-
-      # Markup tells jinja2 that the object is safe for rendering, without
-      # escaping the quotes (caused problems when creating JSON object).
-      format_data = Markup(format_data)
-      # need to redirect instead
-      return render_template('chart.html', stock=stock, data=format_data)
-  elif request.method == 'GET':
-      return render_template('index.html',form=form)
-
-@myApp.before_request
-def csrf_protect():
-	if request.method == 'POST':
-		token = session.pop('_csrf_token', None)
-		if not token or token != request.form.get('_csrf_token'):
-			abort(403)
-
-
-def generate_csrf_token():
-	if '_csrf_token' not in session:
-		session['_csrf_token'] = os.urandom(24).encode('hex')
-	return session['_csrf_token']
-
-myApp.jinja_env.globals['csrf_token'] = generate_csrf_token
+  return render_template('index.html')
