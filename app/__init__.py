62d2ae47a1bd3b81f3475e7f40e915309a75d4df
Grant David Bachman <grantbachman@gmail.com>
2014-06-10 19:20:00 -0400

    add condition to only load instance/config.py if it exists -- errors get thrown on server startup

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,16 @@
 from flask import Flask
+import os.path
 """
 calling it myApp to differentiate it from the package called app -- for my
 benefit. I'm still learning.
 """
 myApp = Flask(__name__, instance_relative_config = True)
 
+# loads the default configuration in chartflux/app/config.py
 myApp.config.from_object('app.config')
-myApp.config.from_pyfile('config.py')
+
+# override app.config with chartflux/instance/config.py if it exists
+if os.path.isfile(os.path.join(myApp.instance_path,'config.py')):
+  myApp.config.from_pyfile('config.py')
+
 from app import views # app refers to the package
