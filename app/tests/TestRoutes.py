91963610a23197073de87e9d6749bee46cb81897
Grant David Bachman <grantbachman@gmail.com>
2014-06-13 23:05:58 -0400

    Pull and graph stock chart data based on HTTP request parameters

--- app/tests/TestRoutes.py
+++ app/tests/TestRoutes.py
@@ -1,12 +1,10 @@
 import unittest
-import sys
 from app import myApp
 
 
 class TestRoutes(unittest.TestCase):
 
   def setUp(self):
-    sys.setrecursionlimit(10000000)
     self.test_app = myApp.test_client()
 
   def test_root(self):
--- app/views.py
+++ app/views.py
@@ -1,4 +1,5 @@
 import sys, os
+import re
 import datetime as dt
 from app import myApp
 from models import Stock
@@ -7,23 +8,28 @@ from flask import render_template, url_for, redirect, request, flash,\
                   session, abort
 
 @myApp.route('/')
-def root():
-  return redirect(url_for('index'))
+@myApp.route('/index')
+def index():
+  return render_template('index.html')
 
 @myApp.route('/d3')
 def d3():
   return render_template('d3.html')
 
 
-@myApp.route('/stock/<stock>')
-def show_stock(stock):
+@myApp.route('/chart')
+def show_stock():
+  stock = request.args.get('ticker')
+  value = request.args.get('value')
+  unit = request.args.get('unit')
   stock = Stock(stock)
   if not stock.is_valid:
     error = "Invalid ticker symbol."
     return render_template('index.html', error = error)
   else:
     end = dt.date.today()
-    start = end - dt.timedelta(days = 1000)
+    td = getTimeDelta(value,unit)
+    start = end - td
     stock.set_data(start, end)
     format_data = stock.data.reset_index()  # DateTimeIndex to column
     format_data = format_data.to_json(date_format='iso', orient='records')
@@ -34,13 +40,25 @@ def show_stock(stock):
     # need to redirect instead
     return render_template('chart.html', stock=stock, data=format_data)
 
+def getTimeDelta(value=1, unit="years"):
+  try:
+    value = int(value)
+  except:
+    value = 1
+  if unit == "days":
+    return dt.timedelta(days = value)
+  elif unit == "weeks":
+    return dt.timedelta(weeks = value)
+  elif unit == "months":
+    return dt.timedelta(days = (value * 30))
+  else:
+    return dt.timedelta(days = (value * 365))
+
+
+
 @myApp.route('/stock/verify', methods = ['POST'])
 def verify_stock():
   ticker = request.form['ticker']
   if ticker == "":
     return redirect(url_for('index'))
-  return redirect(url_for('show_stock', stock=ticker))
-
-@myApp.route('/index')
-def index():
-  return render_template('index.html')
+  return redirect(url_for('show_stock', ticker=ticker))
