7f4e4037e5aba42855579b36030fcacbd84cffb1
Grant David Bachman <grantbachman@gmail.com>
2014-06-18 19:22:42 -0400

    Use symbol.txt to lookup the validity of a searched ticker symbol instead of calling out to Yahoo.

--- app/models.py
+++ app/models.py
@@ -1,6 +1,8 @@
 import datetime
 from pandas import DataFrame
 from pandas.io.data import DataReader
+from app import myApp
+import os
 
 
 class Stock:
@@ -9,6 +11,8 @@ class Stock:
 
 	Instance variables:
 	ticker - Stock's ticker symbol
+	name - Name of the company
+	exchange - Exchange on which the stock is listed.
 	start - The start date of the stock's data
 	end - The end date of the stock's data
 	data - The Pandas Dataframe containing the stock's data
@@ -16,11 +20,24 @@ class Stock:
 
 	def __init__(self, ticker):
 		self.ticker = ticker.upper()
-		if self.get_data() is not None:
+		name_exchange_tuple = self.get_name_and_exchange()
+		if name_exchange_tuple is not None:
+			self.name, self.exchange = name_exchange_tuple
 			self.is_valid = True
 		else:
 			self.is_valid = False
 
+	def get_name_and_exchange(self):
+		symbol_file_path = os.path.join(myApp.root_path,'static/symbols.txt')
+		with open(symbol_file_path, 'r') as inFile:
+				for line in inFile:
+					split = line.strip('\n').split('\t')
+					if split[1] == self.ticker:
+						return (split[2], split[0])
+		return None
+
+
+	# Set's a stock's data without returning anything
 	def set_data(self, start=datetime.date.today() - datetime.timedelta(days=1),
 										 end=datetime.date.today()):
 		self.start = start
--- app/tests/TestStockModel.py
+++ app/tests/TestStockModel.py
@@ -11,11 +11,28 @@ import unittest
 
 class TestStockModel(unittest.TestCase):
 
-	def test_init(self):
+	def test_init_succeed(self):
 		self.stock = Stock("tsla")
 		self.failIf(self.stock.ticker != "TSLA")
 		self.failIf(self.stock.is_valid is not True)
 		self.failUnless(self.stock.is_valid is True)
+
+	def test_init_fail(self):
+		self.stock = Stock("adsfadf")
+		assert(self.stock.is_valid is False)
+
+	def test_get_name_and_exchange_is_success(self):
+		self.stock = Stock("GOOG")
+		return_tuple = self.stock.get_name_and_exchange()
+		assert(return_tuple[0] == "Google Inc.")
+		print return_tuple
+		assert(return_tuple[1] == "NASDAQ")
+
+	def test_get_name_and_exchange_is_failure(self):
+		self.stock = Stock("afdasdf")
+		return_tuple = self.stock.get_name_and_exchange()
+		assert(return_tuple is None)
+
 
 	def test_set_data(self):
 		self.stock = Stock("tsla")
